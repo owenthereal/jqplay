623d1d79c8cbd28f88e8d4ee7fe02731ac599b8e
Jingwen Owen Ou <jingweno@gmail.com>
2014-06-18 00:12:11 -0700

    Add proxy headers

--- server/secure.go
+++ server/secure.go
@@ -9,6 +9,7 @@ func secureMiddleware(c *Config) negroni.Handler {
 	secureMiddleware := secure.New(secure.Options{
 		SSLRedirect:           true,
 		STSSeconds:            315360000,
+		SSLProxyHeaders:       map[string]string{"X-Forwarded-Proto": "https"},
 		STSIncludeSubdomains:  true,
 		FrameDeny:             true,
 		ContentTypeNosniff:    true,
