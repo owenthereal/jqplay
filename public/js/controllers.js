6f0b1a338eb304307ce5de631bfd0cb4a8198713
Jingwen Owen Ou <jingweno@gmail.com>
2014-06-19 23:00:49 -0700

    Remove submit button and automatically update result

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,24 @@
 'use strict';
 
-angular.module('jqplay.controllers', []).controller('JqplayCtrl', function JqplayCtrl($scope, jqplayService) {
+angular.module('jqplay.controllers', []).controller('JqplayCtrl', function JqplayCtrl($scope, $timeout, jqplayService) {
   $scope.result = "";
+
+  $scope.$watch('jq', function(newValue, oldValue) {
+    if ($scope.input.$valid && !angular.equals(newValue, oldValue)) {
+      if ($scope.runTimeout != null) {
+        $timeout.cancel($scope.runTimeout);
+        $scope.runTimeout = null;
+      }
+
+      $scope.runTimeout = $timeout(function() {
+        $scope.run($scope.jq);
+        $scope.runTimeout = null;
+      }, 1000);
+    }
+  }, true);
+
   $scope.run = function(jq) {
-    $scope.result = "";
+    $scope.result = "Loading...";
 
     var promise = jqplayService.run(jq);
     promise.success(function(data) {
