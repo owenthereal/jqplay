512c905f6e5e4334903d3eda667c86c0d80ba1ba
Jingwen Owen Ou <jingweno@gmail.com>
2014-06-27 22:28:06 -0700

    Add capitalize filter

---
+++ public/js/filters.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('jqplay.filters', []).filter('capitalize', function() {
+  return function(input, scope) {
+    if (input == null) {
+      return ''
+    }
+
+    input = input.toLowerCase();
+    input = input.replace('-', ' ');
+
+    var result = [];
+    var str = input.split(' ');
+    for (var i = 0; i < str.length; i++) {
+      result.push(str[i].substring(0, 1).toUpperCase() + str[i].substring(1));
+    }
+
+    return result.join(' ');
+  }
+});
