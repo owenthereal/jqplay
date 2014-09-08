709fec5803526fd1681c39a4e89186cb5d269646
Jingwen Owen Ou <jingweno@gmail.com>
2014-09-07 20:13:34 -0700

    Allow all origin

--- server/cors.go
+++ server/cors.go
@@ -7,20 +7,13 @@ import (
 )
 
 func corsMiddleware(c *Config) negroni.Handler {
-	return &cors{c.AssetHost}
+	return &cors{}
 }
 
 type cors struct {
-	AllowedHost string
 }
 
 func (c *cors) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	if c.AllowedHost != "" {
-		rw.Header().Set("Access-Control-Allow-Origin", "https://jqplay.org")
-		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
-		rw.Header().Set("Access-Control-Allow-Credentials", "true")
-	}
-
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
 	next(rw, r)
 }
