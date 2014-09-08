b173efd31c9e3b19b84ae8da78a3997e81537b34
Jingwen Owen Ou <jingweno@gmail.com>
2014-09-07 20:00:43 -0700

    Test with jqplay.org

--- server/cors.go
+++ server/cors.go
@@ -16,7 +16,7 @@ type cors struct {
 
 func (c *cors) ServeHTTP(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 	if c.AllowedHost != "" {
-		rw.Header().Set("Access-Control-Allow-Origin", c.AllowedHost)
+		rw.Header().Set("Access-Control-Allow-Origin", "https://jqplay.org")
 		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		rw.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
 		rw.Header().Set("Access-Control-Allow-Credentials", "true")
