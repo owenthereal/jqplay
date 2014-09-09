770ab6b60f6aba4198ec0142c3d5f8a8fb3c6245
Jingwen Owen Ou <jingweno@gmail.com>
2014-09-08 21:18:49 -0700

    Support GET /jq?j=xxx&q=xxx

--- server/config.go
+++ server/config.go
@@ -1,7 +1,5 @@
 package server
 
-import "fmt"
-
 type Config struct {
 	Port               string
 	Env                string
@@ -12,8 +10,4 @@ type Config struct {
 
 func (c *Config) IsProduction() bool {
 	return c.Env == "production"
-}
-
-func (c *Config) Asset(path string) string {
-	return fmt.Sprintf("%s/%s", c.AssetHost, path)
 }
