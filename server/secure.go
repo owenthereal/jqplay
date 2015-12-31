eaf1c91dc2238fc91e31cf8b69875aa97c1158ea
Jingwen Owen Ou <jingweno@gmail.com>
2015-12-31 15:17:49 -0800

    Disable CSP…

--- server/secure.go
+++ server/secure.go
@@ -1,24 +1,20 @@
 package server
 
 import (
-	"fmt"
-
 	"github.com/jingweno/jqplay/Godeps/_workspace/src/github.com/codegangsta/negroni"
 	"github.com/jingweno/jqplay/Godeps/_workspace/src/gopkg.in/unrolled/secure.v1"
 )
 
 func secureMiddleware(c *Config) negroni.Handler {
-	csp := fmt.Sprintf("default-src *; script-src 'self' %s https://www.google-analytics.com 'unsafe-inline'; style-src 'self' %s 'unsafe-inline'; object-src 'self' %s;", c.AssetHost, c.AssetHost, c.AssetHost)
 	secureMiddleware := secure.New(secure.Options{
-		SSLRedirect:           true,
-		STSSeconds:            315360000,
-		SSLProxyHeaders:       map[string]string{"X-Forwarded-Proto": "https"},
-		STSIncludeSubdomains:  true,
-		FrameDeny:             true,
-		ContentTypeNosniff:    true,
-		BrowserXssFilter:      true,
-		ContentSecurityPolicy: csp,
-		IsDevelopment:         !c.IsProduction(),
+		SSLRedirect:          true,
+		STSSeconds:           315360000,
+		SSLProxyHeaders:      map[string]string{"X-Forwarded-Proto": "https"},
+		STSIncludeSubdomains: true,
+		FrameDeny:            true,
+		ContentTypeNosniff:   true,
+		BrowserXssFilter:     true,
+		IsDevelopment:        !c.IsProduction(),
 	})
 
 	return negroni.HandlerFunc(secureMiddleware.HandlerFuncWithNext)
