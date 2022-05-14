a95f76eb03a1cd8be1e041c84193d2371f95589c
Owen Ou <o@owenou.com>
2022-05-14 09:19:03 -0700

    Fix up slug id

--- server/db.go
+++ server/db.go
@@ -90,5 +90,12 @@ func slug(s *Snippet) string {
 	b := make([]byte, base64.URLEncoding.EncodedLen(len(sum)))
 	base64.URLEncoding.Encode(b, sum)
 
-	return string(b)[:10]
+	// Web sites don’t always linkify a trailing underscore, making it seem like
+	// the link is broken. If there is an underscore at the end of the substring,
+	// extend it until there is not.
+	hashLen := 11
+	for hashLen <= len(b) && b[hashLen-1] == '_' {
+		hashLen++
+	}
+	return string(b)[:hashLen]
 }
