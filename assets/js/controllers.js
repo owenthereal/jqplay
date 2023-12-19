2c27854aad0e315226a44c3a990b5d5d52e9c70c
Owen Ou <169064+owenthereal@users.noreply.github.com>
2023-12-18 21:45:45 -0800

    Add margin between examples (#168)
    This fixes https://github.com/owenthereal/jqplay/issues/105

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -11,7 +11,7 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
   $scope.cmd = "jq ''";
   $scope.result = "";
 
-  $scope.jsonEditorLoaded = function(_editor) {
+  $scope.jsonEditorLoaded = function (_editor) {
     _editor.$blockScrolling = Infinity
     _editor.setHighlightActiveLine(false);
     _editor.setFontSize(14);
@@ -19,7 +19,7 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
     _editor.session.setUseWorker(false);
   };
 
-  $scope.queryEditorLoaded = function(_editor) {
+  $scope.queryEditorLoaded = function (_editor) {
     _editor.$blockScrolling = Infinity
     _editor.setHighlightActiveLine(false);
     _editor.setFontSize(14);
@@ -28,9 +28,9 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
     _editor.focus()
   };
 
-  $scope.buildCmd = function(jq) {
+  $scope.buildCmd = function (jq) {
     var cmd = 'jq '
-    jq.o.forEach(function(o) {
+    jq.o.forEach(function (o) {
       if (o.enabled) {
         cmd += '--' + o.name + ' '
       }
@@ -40,14 +40,14 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
     return cmd + "'" + q.replace(/'/g, "'\\''") + "'" // use POSIX shell's strong quoting rules
   }
 
-  $scope.$watch('jq', function(newValue, oldValue) {
-    if ( newValue !== oldValue ) {
+  $scope.$watch('jq', function (newValue, oldValue) {
+    if (newValue !== oldValue) {
       $scope.cmd = $scope.buildCmd($scope.jq)
       $scope.delayedRun($scope.jq)
     }
   }, true);
 
-  $scope.shareSnippet = function() {
+  $scope.shareSnippet = function () {
     jqplayService.share($scope.jq).then(
       function successCallback(response) {
         $window.location.href = '/s/' + response.data;
@@ -57,14 +57,14 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
       });
   };
 
-  $scope.delayedRun = function(jq) {
+  $scope.delayedRun = function (jq) {
     if ($scope.input.$valid) {
       if ($scope.runTimeout != null) {
         $timeout.cancel($scope.runTimeout);
         $scope.runTimeout = null;
       }
 
-      $scope.runTimeout = $timeout(function() {
+      $scope.runTimeout = $timeout(function () {
         $scope.run(jq);
         $scope.runTimeout = null;
       }, 1000);
@@ -73,7 +73,7 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
     }
   };
 
-  $scope.run = function(jq) {
+  $scope.run = function (jq) {
     $scope.result = "Loading...";
 
     jqplayService.run(jq).then(function successCallback(response) {
@@ -86,37 +86,37 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
   $scope.samplesLeft = [
     {
       text: "unchanged input",
-      code: ".",
+      code: ["."],
       input_q: ".",
       input_j: '{ "foo": { "bar": { "baz": 123 } } }'
     },
     {
       text: "value at key",
-      code: ".foo, .foo.bar, .foo?",
+      code: [".foo", ".foo.bar", ".foo?"],
       input_q: ".foo",
       input_j: '{"foo": 42, "bar": "less interesting data"}'
     },
     {
       text: "array operation",
-      code: ".[], .[]?, .[2], .[10:15]",
+      code: [".[]", ".[]?", ".[2]", ".[10:15]"],
       input_q: ".[1]",
       input_j: '[{"name":"JSON", "good":true}, {"name":"XML", "good":false}]'
     },
     {
       text: "array/object construction",
-      code: "[], {}",
+      code: ["[]", "{}"],
       input_q: "{user, title: .titles[]}",
       input_j: '{"user":"jqlang","titles":["JQ Primer", "More JQ"]}'
     },
     {
       text: "length of a value",
-      code: "length",
+      code: ["length"],
       input_q: ".[] | length",
       input_j: '[[1,2], "string", {"a":2}, null]'
     },
     {
       text: "keys in an array",
-      code: "keys",
+      code: ["keys"],
       input_q: "keys",
       input_j: '{"abc": 1, "abcd": 2, "Foo": 3}'
     }
@@ -125,43 +125,43 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
   $scope.samplesRight = [
     {
       text: "feed input into multiple filters",
-      code: ",",
+      code: [","],
       input_q: ".foo, .bar",
       input_j: '{ "foo": 42, "bar": "something else", "baz": true}'
     },
     {
       text: "pipe output of one filter to the next filter",
-      code: "|",
+      code: ["|"],
       input_q: ".[] | .name",
       input_j: '[{"name":"JSON", "good":true}, {"name":"XML", "good":false}]'
     },
     {
       text: "input unchanged if foo returns true",
-      code: "select(foo)",
+      code: ["select(foo)"],
       input_q: "map(select(. >= 2))",
       input_j: '[1,5,3,0,7]'
     },
     {
       text: "invoke filter foo for each input",
-      code: "map(foo)",
+      code: ["map(foo)"],
       input_q: "map(.+1)",
       input_j: '[1,2,3]'
     },
     {
       text: "conditionals",
-      code: "if-then-else-end",
+      code: ["if-then-else-end"],
       input_q: 'if . == 0 then "zero" elif . == 1 then "one" else "many" end',
       input_j: '2'
     },
     {
       text: "string interpolation",
-      code: "\\(foo)",
+      code: ["\\(foo)"],
       input_q: '"The input was \\(.), which is one less than \\(.+1)"',
       input_j: '42'
     }
   ];
 
-  $scope.loadSample = function(samples, index) {
+  $scope.loadSample = function (samples, index) {
     $scope.jq.j = samples[index].input_j;
     $scope.jq.q = samples[index].input_q;
     $scope.input.$valid = true;
