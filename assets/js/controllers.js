5c1e8a6bc8f2c63fe46223b73705d04d991bd408
Owen Ou <o@owenou.com>
2024-01-09 20:19:03 -0800

    Trigger run when query & `--null-input` are both not empty

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -58,7 +58,7 @@ angular.module('jqplay.controllers', []).controller('JqplayCtrl', function Jqpla
   };
 
   $scope.delayedRun = function (jq) {
-    if ($scope.input.$valid || $scope.jq.o.filter(f => f.name == "null-input" && f.enabled)) {
+    if ($scope.input.$valid || (jq.q && jq.o.filter(f => f.name === "null-input" && f.enabled).length > 0)) {
       if ($scope.runTimeout != null) {
         $timeout.cancel($scope.runTimeout);
         $scope.runTimeout = null;
