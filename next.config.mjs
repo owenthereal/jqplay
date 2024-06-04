880f983bcaa631c4e9850ecd50bd342d77c6be38
Owen Ou <o@owenou.com>
2024-06-04 13:24:16 -0700

    Get jq-wasm to work

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, options) => {
+        config.experiments = {
+            asyncWebAssembly: true,
+            layers: true,
+        };
+        config.resolve.fallback = {
+            fs: false,
+            path: false,
+            crypto: false
+        };
+
+        patchWasmModuleImport(config, options.isServer);
+
+        return config;
+    },
+};
+
+function patchWasmModuleImport(config, isServer) {
+    config.module.rules.push({
+        test: /\.wasm$/,
+        type: 'asset/resource',
+    });
+
+    // TODO: improve this function -> track https://github.com/vercel/next.js/issues/25852
+    if (isServer) {
+        config.output.webassemblyModuleFilename = './../static/wasm/[modulehash].wasm';
+    } else {
+        config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
+    }
+}
 
 export default nextConfig;
