087db84a23f85e086b6414087bd7b8a7b1dc29de
Owen Ou <o@owenou.com>
2024-06-04 20:30:27 -0700

    More styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,26 @@
 'use client'
 
 import { useState, useRef, useEffect } from 'react';
-import Editor from '@monaco-editor/react';
+import dynamic from 'next/dynamic';
+
+const Select = dynamic(() => import('react-select'), { ssr: false });
+const Editor = dynamic(() => import('@monaco-editor/react'), { ssr: false });
+
+const jqFlagsOptions = [
+  { value: '-c', label: '-c (Compact output)' },
+  { value: '-M', label: '-M (Monochrome output)' },
+  { value: '-r', label: '-r (Raw output)' },
+  { value: '-s', label: '-s (Slurp: read into array)' },
+  { value: '-S', label: '-S (Sort keys)' },
+  { value: '-R', label: '-R (Raw input)' },
+];
 
 export default function Home() {
   const [result, setResult] = useState<string>('');
 
   const jsonRef = useRef<string>('');
   const queryRef = useRef<string>('');
+  const flagsRef = useRef<string[]>([]);
 
   const workerRef = useRef<Worker | null>(null);
   const runTimeoutRef = useRef<NodeJS.Timeout | null>(null);
@@ -33,23 +46,24 @@ export default function Home() {
     };
   }, []);
 
-  const handleJQRun = (json: string, query: string) => {
+  const handleJQRun = (json: string, query: string, flags: string[]) => {
+    clearRunTimeout();
+
     if (json === '' || query === '') {
       setResult('');
-      clearRunTimeout();
-    } else {
-      setResult('Running...');
-      clearRunTimeout();
-      runTimeoutRef.current = setTimeout(() => {
-        runJQ(json, query, [], 5000)
-          .then((result) => {
-            setResult(result);
-          })
-          .catch((error) => {
-            setResult(error.message);
-          });
-      }, 500);
+      return
     }
+
+    setResult('Running...');
+    runTimeoutRef.current = setTimeout(() => {
+      runJQ(json, query, flags, 5000)
+        .then((result) => {
+          setResult(result);
+        })
+        .catch((error) => {
+          setResult(error.message);
+        });
+    }, 500);
   };
 
   const runJQ = (json: string, query: string, flags: string[], timeout: number): Promise<string> => {
@@ -67,7 +81,13 @@ export default function Home() {
 
       worker.onmessage = function (event) {
         clearTimeout(timeoutId);
-        resolve(event.data.result);
+
+        if (event.data.error) {
+          reject(new Error(event.data.error));
+        } else {
+          resolve(event.data.result);
+        }
+
         worker.terminate();
         workerRef.current = null;
       };
@@ -86,22 +106,52 @@ export default function Home() {
   const handleQueryEditorChange = (value: string | undefined) => {
     if (value !== undefined) {
       queryRef.current = value;
-      handleJQRun(jsonRef.current, queryRef.current);
+      handleJQRun(jsonRef.current, queryRef.current, flagsRef.current);
     }
   };
 
   const handleJSONEditorChange = (value: string | undefined) => {
     if (value !== undefined) {
       jsonRef.current = value;
-      handleJQRun(jsonRef.current, queryRef.current);
+      handleJQRun(jsonRef.current, queryRef.current, flagsRef.current);
     }
   };
 
+  const handleFlagsChange = (selectedOptions: any) => {
+    console.log(selectedOptions)
+    flagsRef.current = selectedOptions ? selectedOptions.map((option: any) => option.value) : [];
+    console.log(flagsRef.current)
+    handleJQRun(jsonRef.current, queryRef.current, flagsRef.current);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Editor height="20vh" width="100%" defaultLanguage="plaintext" onChange={handleQueryEditorChange} />
-      <Editor height="50vh" width="100%" defaultLanguage="json" onChange={handleJSONEditorChange} />
-      <Editor height="50vh" width="100%" defaultLanguage="json" value={result} options={{ readOnly: true }} />
-    </main>
+    <div className="flex flex-col items-center justify-center min-h-screen p-6 bg-gray-100">
+      <h1 className="text-4xl font-bold mb-8">jqplay</h1>
+      <div className="flex flex-col md:flex-row md:space-x-6 w-full max-w-7xl">
+        <div className="w-full md:w-1/2 mb-6 md:mb-0">
+          <h2 className="text-xl font-semibold mb-2">JSON</h2>
+          <Editor height="40vh" width="100%" defaultLanguage="json" onChange={handleJSONEditorChange} />
+        </div>
+        <div className="w-full md:w-1/2 mb-6 md:mb-0">
+          <h2 className="text-xl font-semibold mb-2">Filter</h2>
+          <Editor height="40vh" width="100%" defaultLanguage="plaintext" onChange={handleQueryEditorChange} />
+        </div>
+      </div>
+      <div className="w-full max-w-7xl mb-6">
+        <h2 className="text-xl font-semibold mb-2">Options</h2>
+        <Select
+          isMulti
+          options={jqFlagsOptions}
+          onChange={handleFlagsChange}
+          className="basic-multi-select"
+          classNamePrefix="select"
+          placeholder="Select jq flags"
+        />
+      </div>
+      <div className="w-full max-w-7xl">
+        <h2 className="text-xl font-semibold mb-2">Output</h2>
+        <Editor height="40vh" width="100%" defaultLanguage="json" value={result} options={{ readOnly: true }} />
+      </div>
+    </div>
   );
 }
--- workers/worker.ts
+++ workers/worker.ts
@@ -2,11 +2,12 @@ import jq from 'jq-wasm';
 
 addEventListener('message', (event: any) => {
     const { json, query, flags } = event.data;
+
     jq.raw(json, query, flags)
         .then((result: string) => {
             postMessage({ result: result });
         })
         .catch((error: any) => {
-            postMessage({ result: error.toString() });
+            postMessage({ error: error.toString() });
         });
 });
