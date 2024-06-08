2bce404fa59d1f6f270490e6c628a77082d22603
Owen Ou <o@owenou.com>
2024-06-07 19:51:43 -0700

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest"></link>
+      </head>
       <body className={inter.className}>
         {children}
       </body>
