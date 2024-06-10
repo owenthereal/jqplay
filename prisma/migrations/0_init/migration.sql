96aa39ef155da455d3261a951b1bf6c63336baec
Owen Ou <o@owenou.com>
2024-06-09 23:09:58 -0700

    Add prisma migration

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateExtension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- CreateTable
+CREATE TABLE "snippets" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "slug" TEXT NOT NULL,
+    "json" TEXT NOT NULL,
+    "query" TEXT NOT NULL,
+    "options" TEXT[],
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "snippets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "snippets_slug_key" ON "snippets"("slug");
+
