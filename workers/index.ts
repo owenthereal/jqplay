25c11c03f01550f7f9ed6b262d86310452b14d9d
Owen Ou <o@owenou.com>
2024-06-20 21:30:15 -0700

    Target at ES6

--- workers/index.ts
+++ workers/index.ts
@@ -2,19 +2,19 @@ import * as Comlink from "comlink";
 import type { Worker as WorkerInterface } from "./process";
 
 export class JQWorker {
-    worker: Comlink.Remote<WorkerInterface>;
-    webWorker: Worker;
+    #worker: Comlink.Remote<WorkerInterface>;
+    #webWorker: Worker;
 
     constructor() {
-        this.webWorker = new Worker(new URL("./process.ts", import.meta.url), { type: "module" });
-        this.worker = Comlink.wrap<WorkerInterface>(this.webWorker);
+        this.#webWorker = new Worker(new URL("./process.ts", import.meta.url), { type: "module" });
+        this.#worker = Comlink.wrap<WorkerInterface>(this.#webWorker);
     }
 
     jq(json: string, query: string, options: any): Promise<string> {
-        return this.worker.jq(json, query, options);
+        return this.#worker.jq(json, query, options);
     }
 
     terminate() {
-        this.webWorker.terminate();
+        this.#webWorker.terminate();
     }
 }
