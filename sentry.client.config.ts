5f87a515c74a3468950e2f781e492c2ba1ff1457
Owen Ou <o@owenou.com>
2024-08-16 10:08:00 -0700

    Reduce Sentry tracing rate

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -8,16 +8,16 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || "",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0.1,
+  tracesSampleRate: 0.05,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  replaysOnErrorSampleRate: 0.1,
+  replaysOnErrorSampleRate: 0.05,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0.05,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -9,7 +9,7 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || "",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.05,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
